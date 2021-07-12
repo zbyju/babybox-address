6b27f0926204dcf8cb9fe4e8b7cbb3538f7c66c0
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-07-12 21:06:54 +0200

    Add links to all relevant pages from babybox page

--- client/pages/babybox/[BabyboxPage].tsx
+++ client/pages/babybox/[BabyboxPage].tsx
@@ -1,8 +1,9 @@
-import {Heading, HStack} from "@chakra-ui/react";
+import {Box, Button, ButtonGroup, Heading, HStack, VStack} from "@chakra-ui/react";
 import {useRouter} from "next/router";
 import {getDefaultBabybox} from "../../utils/defaultFactory";
 import {useState} from "react";
 import BabyboxStats from "../../components/Babybox/Stats/BabyboxStats";
+import {AddIcon, ArrowRightIcon, ChevronLeftIcon, DownloadIcon, InfoIcon} from "@chakra-ui/icons";
 
 export default function BabyboxPage() {
     const router = useRouter();
@@ -12,6 +13,27 @@ export default function BabyboxPage() {
         <>
             <Heading>Babybox {babybox.name ? babybox.name : handle}</Heading>
             <BabyboxStats babyboxHandle={handle} />
+
+            <Box bg="yellow.300" borderRadius={5} mt={5} boxShadow="lg">
+                <Box p={5}>
+                    <Heading size="lg" mb={4}>Rozcestník</Heading>
+                    <HStack spacing="16px">
+                        <ButtonGroup colorScheme="blue" spacing="2" size="sm" color="white">
+                            <Button bg="blue.900" leftIcon={<ChevronLeftIcon w={6} h={6}/>} pl={2}>Otevřít databázi adres</Button>
+                            <Button bg="blue.700" leftIcon={<AddIcon />}>Přidávat další adresy</Button>
+                        </ButtonGroup>
+
+                        <ButtonGroup spacing="2" size="sm">
+                            <Button leftIcon={<InfoIcon />} isDisabled>Pokročilé statistiky</Button>
+                        </ButtonGroup>
+
+                        <ButtonGroup colorScheme="green" spacing="2" size="sm" color="white">
+                            <Button bg="green.900" leftIcon={<DownloadIcon />}>Stáhnout zálohu dat</Button>
+                            <Button bg="green.700" leftIcon={<ArrowRightIcon />}>Exportovat data</Button>
+                        </ButtonGroup>
+                    </HStack>
+                </Box>
+            </Box>
         </>
     )
 }
