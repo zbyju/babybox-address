b80b9db71d73dcb0fe125dbb93d0e0d3b8509f5d
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-30 01:02:09 +0200

    Add func to fetch babybox by handle

--- client/api/babybox/getBabyboxes.ts
+++ client/api/babybox/getBabyboxes.ts
@@ -1,12 +1,22 @@
 import { Babybox } from '../../types/babybox'
 import { config } from '../../config'
 import { getRequest } from '../fetch'
+import { getDefaultBabybox } from '../../utils/defaultFactory'
 
 export const getAllBabyboxes = async () => {
     try {
       const result = await getRequest<any>("babybox")
-      return await result.babyboxes
+      return result.babyboxes
     } catch(err) {
       return []
     }
+}
+
+export const getBabyboxByHandle = async (handle: string) => {
+  try {
+    const result = await getRequest<any>("babybox/handle/" + handle)
+    return result.babybox
+  } catch(err) {
+    return getDefaultBabybox()
+  }
 }
