7ebcb8b031fb1709303741f4016a24e1275f521c
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-30 16:55:00 +0200

    Remove mock data from index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,49 +1,27 @@
-import {Button, Flex, Heading, IconButton} from "@chakra-ui/react";
+import { Button, Flex, Heading, IconButton } from "@chakra-ui/react";
 import { AddIcon } from '@chakra-ui/icons'
 import AllBabyboxesCardList from "../components/Babybox/AllBabyboxesCardList";
-import {Babybox} from "../types/babybox";
+import { Babybox } from "../types/babybox";
 import Link from "next/link"
-import {useState} from "react";
+import { useState } from "react";
 import ListFastLinks from "../components/Other/ListFastLinks";
 import useSWR from 'swr'
 import { fetcher } from "../api/fetcher";
 
 export default function Home() {
-    // const {data, error} = useSWR("http://localhost:8080/babybox", fetcher)
-    const [babyboxesTmp, setBabyboxesTmp] = useState(    [{
-        name: "Praha",
-        handle: "praha",
-        favorite: false
-    },{
-        name: "Brno",
-        handle: "brno",
-        favorite: false
-    },{
-        name: "Ostrava",
-        handle: "ostrava",
-        favorite: true,
-        note: "Toto je poznamka k tomuto babyboxu"
-    }]);
     const fastLinks = [{
         label: "Google",
         link: "https://google.com"
-    },{
+    }, {
         label: "Justice",
         link: "https://justice.cz"
-    },{
+    }, {
         label: "Test",
         link: "https://google.com"
-    },{
+    }, {
         label: "zvcx",
         link: "https://google.com"
     },]
-
-    // const babyboxesJSX = error ? (
-    //     <p>Žádné babyboxy.</p>
-    // ) : !data ? (
-    //     <p>Načítám...</p>
-    // ) : <AllBabyboxesCardList babyboxesProp={data.babyboxes} />
-    const babyboxesJSX = <AllBabyboxesCardList babyboxesProp={[]} />
     return (
         <>
             <Flex alignItems="center" mb={5}>
@@ -53,10 +31,10 @@ export default function Home() {
                 </Link>
             </Flex>
 
-            {babyboxesJSX}
+            <AllBabyboxesCardList />
 
             <ListFastLinks linksProp={fastLinks} />
         </>
-      )
+    )
 
 }
