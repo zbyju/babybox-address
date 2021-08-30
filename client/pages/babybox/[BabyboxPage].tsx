c92cf5f67f17b0fab5e685250f3b14a4906c9d02
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-30 22:51:56 +0200

    Make button tp open add form work on babyboxpage

--- client/pages/babybox/[BabyboxPage].tsx
+++ client/pages/babybox/[BabyboxPage].tsx
@@ -6,6 +6,7 @@ import BabyboxStats from "../../components/Babybox/Stats/BabyboxStats";
 import { AddIcon, ArrowRightIcon, ChevronLeftIcon, DownloadIcon, InfoIcon } from "@chakra-ui/icons";
 import useSWR from "swr";
 import { getBabyboxByHandle } from "../../api/babybox/getBabyboxes";
+import Link from "next/link"
 
 export default function BabyboxPage() {
     const router = useRouter();
@@ -22,7 +23,9 @@ export default function BabyboxPage() {
                     <HStack wrap="wrap" spacing="0">
                         <ButtonGroup spacing="0" colorScheme="blue" size="sm" color="white">
                             <Button mt="5px" mr="10px" bg="blue.900" leftIcon={<ChevronLeftIcon w={6} h={6} />} pl={1}>Otevřít databázi adres</Button>
-                            <Button mt="5px" mr="10px" bg="blue.700" leftIcon={<AddIcon />}>Přidávat další adresy</Button>
+                            <Link href={`/babybox/address/add/${encodeURIComponent(handle)}`}>
+                                <Button mt="5px" mr="10px" bg="blue.700" leftIcon={<AddIcon />}>Přidávat další adresy</Button>
+                            </Link>
                         </ButtonGroup>
 
                         <ButtonGroup spacing="0" size="sm">
