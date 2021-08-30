d21f161750acaa2e3523be1497368b86122de2e9
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-30 16:54:06 +0200

    Add nicer error and loading

--- client/components/Babybox/AllBabyboxesCardList.tsx
+++ client/components/Babybox/AllBabyboxesCardList.tsx
@@ -1,18 +1,14 @@
-import {Box, Flex, HStack, Stack, Wrap, WrapItem} from "@chakra-ui/react";
+import { Box, Flex, SkeletonCircle, SkeletonText, Alert, AlertIcon, AlertDescription, AlertTitle, Heading, HStack, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 import BabyboxCard from "./BabyboxCard"
-import {Babybox} from "../../types/babybox";
-import {useState} from "react";
+import { Babybox } from "../../types/babybox";
+import React, { useState } from "react";
 import { updateFavorite } from "../../api/babybox/updateFavorite";
 import useSWR from 'swr'
 import { fetcher } from "../../api/fetcher";
 import { getAllBabyboxes } from "../../api/babybox/getBabyboxes";
 
-interface BabyboxesListProps {
-    babyboxesProp: Array<Babybox>
-}
-
-export default function AllBabyboxesCardList({ babyboxesProp }: BabyboxesListProps) {
-    const {data: babyboxes, error, mutate} = useSWR("babyboxes", () => getAllBabyboxes())
+export default function AllBabyboxesCardList() {
+    const { data: babyboxes, error, mutate } = useSWR("babyboxes", () => getAllBabyboxes())
     const toggleFavoriteStar = async (babybox: Babybox) => {
         try {
             const newBabybox = await updateFavorite(babybox)
@@ -20,25 +16,40 @@ export default function AllBabyboxesCardList({ babyboxesProp }: BabyboxesListPro
                 return x.handle === newBabybox.handle ? newBabybox : x
             })
             mutate(tmp)
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
     const sortBabyboxes = (a: Babybox, b: Babybox) => {
-        if(a.favorite && !b.favorite) return -1
-        if(!a.favorite && b.favorite) return 1
-        if(a.name > b.name) return 1
-        if(a.name < b.name) return -1
+        if (a.favorite && !b.favorite) return -1
+        if (!a.favorite && b.favorite) return 1
+        if (a.name > b.name) return 1
+        if (a.name < b.name) return -1
         return 0
     }
-    if(error) return <span>Chyba</span>
-    if(!babyboxes) return <span>Nacitam</span>
+    if (error) return (
+        <Alert status="error" mb="3">
+            <AlertIcon />
+            <AlertTitle mr={2}>Chyba při načítání babyboxů.</AlertTitle>
+            <AlertDescription>Zkuste stránku obnovit, pokud problém přetrvává, pak je problém se serverem.</AlertDescription>
+        </Alert>
+    )
+    if (!babyboxes) return (
+        <Box boxShadow="lg"
+            borderRadius="md"
+            p={5} bg="yellow.300"
+            mr={5} mb={3}
+            maxW="275px">
+            <SkeletonCircle size="10" />
+            <SkeletonText mt="4" noOfLines={4} spacing="4" />
+        </Box>
+    )
     return (
         <HStack spacing="0px" wrap="wrap" shouldWrapChildren>
             {babyboxes.sort(sortBabyboxes).map((babybox: Babybox) => {
                 return (
                     <BabyboxCard babybox={babybox} key={babybox.handle}
-                                 starPressed={toggleFavoriteStar} />
+                        starPressed={toggleFavoriteStar} />
                 )
             })}
         </HStack>
