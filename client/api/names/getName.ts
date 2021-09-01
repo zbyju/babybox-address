3b39de4d13f40eec829ee333f6b6fd18ead59ddb
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-01 19:39:08 +0200

    Update api names to correctly get data

--- client/api/names/getName.ts
+++ client/api/names/getName.ts
@@ -2,10 +2,10 @@ import { getRequest } from "../fetch"
 
 export const getFirstname = async (case1: string) => {
   const result = await getRequest<any>("firstname/" + case1)
-  return result.name
+  return result
 }
 
 export const getLastname = async (case1: string) => {
   const result = await getRequest<any>("lastname/" + case1)
-  return result.name
+  return result
 }
