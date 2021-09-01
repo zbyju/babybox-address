5668cbb4c96e2e8cc3d8a5b4c4ec6cb2b87e34b9
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-01 19:39:19 +0200

    Add axios baseURL

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,13 +3,19 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Nav from '../components/Navbar/Nav'
 import PageLayout from '../components/PageLayout'
+import axios from 'axios'
+import { SWRConfig } from 'swr'
+
+axios.defaults.baseURL = 'http://localhost:8080';
 
 function App(props: AppProps) {
   return (
+    <SWRConfig value={{ fetcher: (url: string) => axios(url).then(r => r.data) }}>
       <ChakraProvider resetCSS={true}>
-          <Nav />
-          <PageLayout props={props} />
+        <Nav />
+        <PageLayout props={props} />
       </ChakraProvider>
+    </SWRConfig >
   )
 }
 export default App
