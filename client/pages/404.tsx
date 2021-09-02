8a7d9268bf87d3595e4beee2f2a2785acab87406
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-02 04:23:17 +0200

    Add custom 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,11 @@
+import { VStack, Heading } from "@chakra-ui/react";
+
+export default function Custom404() {
+  return (
+    <>
+      <VStack minH="80vh">
+        <Heading size="lg" fontWeight="100" textAlign="center" margin="auto">404 - Tato stránka neexistuje</Heading>
+      </VStack>
+    </>
+  )
+}
