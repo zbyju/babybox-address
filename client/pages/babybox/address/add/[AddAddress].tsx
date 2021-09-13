e98c9e5b022062078ad78870a5ceffe5cb0c3088
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-13 19:47:02 +0200

    Add address table below add form

--- client/pages/babybox/address/add/[AddAddress].tsx
+++ client/pages/babybox/address/add/[AddAddress].tsx
@@ -1,26 +1,38 @@
-import { Heading, HStack, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { Heading, HStack, ListItem, Text, UnorderedList, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import { getAddressCount } from "../../../../api/address/getAddresses";
 import AddAddressForm from "../../../../components/Babybox/AddAddressForm"
+import AddressTable from "../../../../components/Babybox/AddressTable";
+import AddressesCountStat from "../../../../components/Babybox/Stats/AddressesCountStat";
 
 export default function AddAddress() {
     const router = useRouter();
     const { AddAddress: handle } = Array.isArray(router.query) ? router.query[0] : router.query
-    const { data, error } = useSWR(handle ? "/address/count/handle/" + handle : null)
-
-    const addressCountJSX = error ? (
-        <Heading size="md">Chyba při načítání počtu adres</Heading>
-    ) : (!data) ? (
-        <Heading size="md">Načítám počet adres...</Heading>
-    ) : (<Heading size="md">Počet adres: {data.count}</Heading>)
+    const { data, error } = useSWR(handle ? "/address/babybox/handle/" + handle : null)
     return (
         <>
             <HStack justify="space-between">
                 <Heading>Přidat adresu</Heading>
-                {addressCountJSX}
+                {error ? (
+                    <Heading size="md">Chyba při načítání počtu adres</Heading>
+                ) : !data ? (
+                    <Heading size="md">Načítám počet adres...</Heading>
+                ) : (
+                    <Heading size="md">Počet adres: {data.count}</Heading>
+                )}
             </HStack>
             <AddAddressForm babyboxHandle={handle} />
+            <VStack alignItems="flex-start" mb="10">
+                <Heading>Nejnovější adresy</Heading>
+                {error ? (
+                    <Text>Chyba při načítání adres</Text>
+                ) : !data ? (
+                    <Text>Načítám adresy...</Text>
+                ) : (
+                    <AddressTable handle={handle} addresses={data.slice(0, 5)}/>
+                )}
+            </VStack>
         </>
     )
 }
