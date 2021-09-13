898fe92bfa6a77bf4a11825788d7993fb444bf37
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-09-13 20:10:23 +0200

    Fix button labels

--- client/components/Address/BrowseAddressesActions.tsx
+++ client/components/Address/BrowseAddressesActions.tsx
@@ -36,11 +36,15 @@ export default function BrowseAddressesActions({ address, handle, addresses, set
 
       <Button size="sm" isDisabled={!address}
         color="gray.700" bg="yellow.200" _hover={{ bg: "yellow.300" }}
-        minW="135px" onClick={() => inverseDonor(address)} >Označit jako dárce</Button>
+        minW="135px" onClick={() => inverseDonor(address)} >
+          {address.flags?.isDonor === true ? "Označit jako nedárce" : "Označit jako dárce"}
+      </Button>
 
       <Button size="sm" isDisabled={!address}
         color="gray.700" bg="yellow.300" _hover={{ bg: "yellow.400" }}
-        minW="115px" onClick={() => inverseEmail(address)} >Email odeslán</Button>
+        minW="115px" onClick={() => inverseEmail(address)} >
+          {address.flags?.isEmailSent === true ? "Označit email neodeslán" : "Označit email odeslán"}
+      </Button>
 
       <EditAddressModal address={editDialog.address} handle={handle} isOpen={editDialog.open} close={() => setEditDialog({ open: false })} />
     </HStack>
