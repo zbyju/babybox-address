4574834891a80b6374e0c1b03fadb07ae6c24667
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-11-20 22:22:02 +0100

    Fix babybox page button positioning

--- client/pages/babybox/[BabyboxPage].tsx
+++ client/pages/babybox/[BabyboxPage].tsx
@@ -1,13 +1,9 @@
-import { Box, Button, ButtonGroup, Heading, HStack, VStack } from "@chakra-ui/react";
+import { AddIcon, ArrowRightIcon, ChevronLeftIcon, DownloadIcon, SearchIcon } from "@chakra-ui/icons";
+import { Box, Button, Heading, HStack } from "@chakra-ui/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { getDefaultBabybox } from "../../utils/defaultFactory";
-import { useState } from "react";
-import BabyboxStats from "../../components/Babybox/Stats/BabyboxStats";
-import { AddIcon, ArrowRightIcon, SearchIcon, ChevronLeftIcon, DownloadIcon, InfoIcon } from "@chakra-ui/icons";
 import useSWR from "swr";
-import { getBabyboxByHandle } from "../../api/babybox/getBabyboxes";
-import Link from "next/link"
-import moment from "moment";
+import BabyboxStats from "../../components/Babybox/Stats/BabyboxStats";
 import { getFilename } from "../../utils/backup";
 
 export default function BabyboxPage() {
@@ -21,35 +17,34 @@ export default function BabyboxPage() {
 
             <Box bg="yellow.300" borderRadius={5} mt={5} boxShadow="lg">
                 <Box p={5}>
-                    <Heading size="lg" mb={4}>Rozcestník</Heading>
-                    <HStack wrap="wrap" spacing="0">
-                        <ButtonGroup spacing="0" colorScheme="blue" size="sm" color="white">
+                    <Heading size="lg" mb={2}>Rozcestník</Heading>
+                    <HStack wrap="wrap">
+                        <HStack wrap="wrap" justifyItems="flex-start" alignItems="center">
                             <Link href={`/babybox/address/${encodeURIComponent(handle)}`} passHref>
-                                <Button mt="5px" mr="10px" bg="blue.900" leftIcon={<ChevronLeftIcon w={6} h={6} />} pl={1}>Otevřít databázi adres</Button>
+                                <Button size="sm" mx="0" my="1" bg="blue.900" _hover={{bg: "blue.800"}} color="white" leftIcon={<ChevronLeftIcon w={6} h={6} />} pl={1}>Otevřít databázi adres</Button>
                             </Link>
                             <Link href={`/babybox/address/add/${encodeURIComponent(handle)}`} passHref>
-                                <Button mt="5px" mr="10px" bg="blue.700" leftIcon={<AddIcon />}>Přidávat další adresy</Button>
+                                <Button size="sm" mx="0" my="1" bg="blue.700" _hover={{bg: "blue.600"}} color="white" leftIcon={<AddIcon />}>Přidávat další adresy</Button>
                             </Link>
                             <Link href={`/babybox/address/browse/${encodeURIComponent(handle)}`} passHref>
-                                <Button mt="5px" mr="10px" bg="blue.600" leftIcon={<SearchIcon />}>Procházet adresy</Button>
+                                <Button size="sm" mx="0" my="1" bg="blue.600" _hover={{bg: "blue.500"}} color="white" leftIcon={<SearchIcon />}>Procházet adresy</Button>
                             </Link>
-                        </ButtonGroup>
 
-                        {/* <ButtonGroup spacing="0" size="sm">
-                            <Button mt="5px" mr="10px" leftIcon={<InfoIcon />} isDisabled>Pokročilé statistiky</Button>
-                        </ButtonGroup> */}
-
-                        <ButtonGroup ml="0px" spacing="0" colorScheme="green" size="sm" color="white">
+                            {/* <ButtonGroup spacing="0" size="sm">
+                                <Button mt="5px" mr="10px" leftIcon={<InfoIcon />} isDisabled>Pokročilé statistiky</Button>
+                            </ButtonGroup> */}
+                        </HStack>
+                        <HStack wrap="wrap" justifyItems="flex-start" alignItems="center">
                             <a href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(addresses))}`}
                                 download={getFilename("ZALOHA", handle, "json")}>
-                                <Button mt="5px" mr="10px" bg="green.900" leftIcon={<DownloadIcon />}>
+                                <Button size="sm" mx="0" my="1" bg="green.900" _hover={{bg: "green.800"}} color="white" leftIcon={<DownloadIcon />}>
                                     Stáhnout zálohu dat
                                 </Button>
                             </a>
                             <Link href={`/babybox/address/export/${encodeURIComponent(handle)}`} passHref>
-                                <Button mt="5px" mr="10px" bg="green.700" leftIcon={<ArrowRightIcon />}>Exportovat data</Button>
+                                <Button size="sm" mx="0" my="1" bg="green.700" _hover={{bg: "green.600"}} color="white" leftIcon={<ArrowRightIcon />}>Exportovat data</Button>
                             </Link>
-                        </ButtonGroup>
+                        </HStack>
                     </HStack>
                 </Box>
             </Box>
