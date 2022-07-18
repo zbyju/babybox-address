c83bf70b4f71c48b055c18b790f34d360a5155bf
zbyju <zbynek.jurica@gmail.com>
2022-07-18 11:51:36 +0200

    Change email and company to be unique with babyboxId

--- server/src/models/addressModel.ts
+++ server/src/models/addressModel.ts
@@ -1,27 +1,33 @@
-import {Schema, model, PopulatedDoc, Document} from 'mongoose';
-import { Address } from '../types/address.types'
-import {Babybox} from "../types/babybox.types";
+import { Schema, model, PopulatedDoc, Document } from "mongoose";
+import { Address } from "../types/address.types";
+import { Babybox } from "../types/babybox.types";
 
-const schema = new Schema<Address>({
-    titleInFront: {type: String, trim: true},
-    titleBehind: {type: String, trim: true},
-    firstname: { type: String, required: true, trim: true},
-    lastname: { type: String, required: true, trim: true},
-    sex: { type: String, required: true},
-    firstname5: { type: String, required: true, trim: true},
-    lastname5: { type: String, required: true, trim: true},
-    email: { type: String, unique: true, index: true, sparse: true, trim: true },
-    company: { type: String, required: true, unique: true, index: true, trim: true },
-    street: { type: String, required: true, trim: true},
-    city: { type: String, required: true, trim: true},
-    postcode: { type: String, required: true, trim: true},
-    babyboxId: { type: Schema.Types.ObjectId, ref: 'Babybox', index: true },
+const schema = new Schema<Address>(
+  {
+    titleInFront: { type: String, trim: true },
+    titleBehind: { type: String, trim: true },
+    firstname: { type: String, required: true, trim: true },
+    lastname: { type: String, required: true, trim: true },
+    sex: { type: String, required: true },
+    firstname5: { type: String, required: true, trim: true },
+    lastname5: { type: String, required: true, trim: true },
+    email: { type: String, index: true, sparse: true, trim: true },
+    company: { type: String, required: true, index: true, trim: true },
+    street: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    postcode: { type: String, required: true, trim: true },
+    babyboxId: { type: Schema.Types.ObjectId, ref: "Babybox", index: true },
     flags: {
-        isEmailSent: { type: Boolean, default: false },
-        isDonor: { type: Boolean, default: false },
-        isMayor: { type: Boolean, default: false },
+      isEmailSent: { type: Boolean, default: false },
+      isDonor: { type: Boolean, default: false },
+      isMayor: { type: Boolean, default: false },
     },
-    donated: { type: Number }
-}, { timestamps: true });
+    donated: { type: Number },
+  },
+  { timestamps: true }
+);
 
-export const AddressModel = model<Address>('Address', schema);
+schema.index({ babyboxId: 1, email: 1 }, { unique: true });
+schema.index({ babyboxId: 1, company: 1 }, { unique: true });
+
+export const AddressModel = model<Address>("Address", schema);
